e007645d09de2ea6b10e3f64bfd017ee4ee50333
MCPolichette <mark@polichette.com>
2018-09-02 16:24:35 -0600

    finally getting things to populate..  this is tough

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import React, { Component } from 'react';
+// import React
+import React, { Component } from "react";
+// import component files
+import IconCard from "./components/IconCard";
 
-import './App.css';
+// import JSON file containing card-data
+import cards from "./cards.json";
+// import CSS file.
+import "./App.css";
+import FaceCard from "./components/IconCard/IconCard";
 
+// establish global variables
+let currentScore = 0;
+let topScore = 0;
+
+// Write up Class App.
 class App extends Component {
+  // Establish state at the beginning of the class
+  state = {
+    cards,
+    currentScore,
+    topScore
+  };
+
+  // onClick function
+  // if else statement, ifs:
+  // matches already clicked,
+  // doesnt match click,
+  // win scenario
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="jumbotron">
+          <h1 className="display-4">TEST</h1>
+          <h3>Correct Guesses: {this.state.currentScore}</h3>
+          <h3>Top Score: {this.state.topScore}</h3>
+          <button
+            onClick={console.log("cra")}
+            className="btn btn-success"
+            id="click"
+          >
+            #
+          </button>
+        </div>
+        <div className="Container">
+          <div className="row">
+            <div className="col-md-6">
+              {this.state.cards.map(card => (
+                <IconCard id={cards.id} key={cards.id} image={cards.image} />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/components/IconCard/IconCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./IconCard.css";
+
+const IconCard = props => (
+  <div className="card">
+    <div className="face-container">
+      <h1 className="display-1">X</h1>
+    </div>
+  </div>
+);
+export default IconCard;
